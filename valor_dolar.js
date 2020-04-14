a0b889a497dd8faf657589ff40376f1b78503671
Agustin Moreno <agustin@theeye.io>
2020-04-14 14:14:37 -0300

    fix json

--- valor_dolar.js
+++ valor_dolar.js
@@ -168,8 +168,7 @@ const dataOutput = async () => {
                     //const nose2 = JSON.parse(nose)
                     console.log(putJSONData)
                     
-
-                    fs.writeFileSync("C:\\deploys\\valor_dolar\\filedownload\\"+positionInColumns+'.json', putJSONData)
+                    fs.appendFileSync("C:\\deploys\\valor_dolar\\filedownload\\valor_dolar_"+processParams.fecha+'.json', putJSONData)
                 }
                 browser.close()
         } catch (err) {
